107d9ed945f8055dae7b17a9bbaa712a117dfcce
Tien Vo <nthny.vo@gmail.com>
2021-12-29 22:44:55 -0700

    Rename repo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="mpl_extras",
     version="0.0.1",
     description="Format for matplotlib",
-    url="https://github.com/anthony-vo/mpl_extras.git",
+    url="https://github.com/tien-vo/mpl_extras.git",
     packages=find_packages(),
     python_requires=">=3.7,<4",
     install_requires=["matplotlib"],
